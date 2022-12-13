9820e25d82697592c0c8bc5eb9b12c4a6ceba485
Phạm Quang Trung <51282533+phamcham@users.noreply.github.com>
2022-12-13 04:51:02 +0000

    Pending changes exported from your codespace

--- Scripts/Editor/Core/AnimationControllerDefaults.cs
+++ Scripts/Editor/Core/AnimationControllerDefaults.cs
@@ -8,7 +8,7 @@ namespace BrunoMikoski.AnimationSequencer
     public sealed class AnimationControllerDefaults : EditorDefaultResourceSingleton<AnimationControllerDefaults>
     {
         [SerializeField]
-        private CustomEase defaultEasing = CustomEase.Unset;
+        private CustomEase defaultEasing = CustomEase.InOutQuad;
         public CustomEase DefaultEasing => defaultEasing;
 
         [SerializeField]
